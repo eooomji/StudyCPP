54a20939296c9c4a3705542a361f0bcd98e0f410
eooomji <dcs0909@naver.com>
2021-02-18 15:28:57 +0900

    5개 정수 입력 받고, 합 출력

---
+++ Chap01_R/문제1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1, num2, num3, num4, num5;
+
+	cout << "1번째 정수 입력: ";
+	cin >> num1;
+
+	cout << "2번째 정수 입력: ";
+	cin >> num2;
+
+	cout << "3번째 정수 입력: ";
+	cin >> num3;
+
+	cout << "4번째 정수 입력: ";
+	cin >> num4;
+
+	cout << "5번째 정수 입력: ";
+	cin >> num5;
+
+	result = num1 + num2 + num3 + num4 + num5;
+
+	cout << "합계: " << result << endl;
+}
