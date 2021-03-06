72d789fb57da3aee374547fcf6871394d879aa12
eooomji <70639677+eooomji@users.noreply.github.com>
2021-03-06 12:33:22 +0900

    임시객체의 복사와 일반

---
+++ Chap05_R/ReturnObjCopycon.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class SoSimple {
+private:
+	int num;
+public:
+	SoSimple(int n) : num(n) {}
+
+	SoSimple(const SoSimple& copy) : num(copy.num) {
+		cout << "Called SoSimple(const SoSimple& copy)" << endl;
+	}
+	
+	SoSimple& AddNum(int n) {
+		num += n;
+		return *this;
+	}
+
+	void ShowData() {
+		cout << "num: " << num << endl;
+	}
+};
+
+SoSimple SimpleFuncObj(SoSimple ob) {
+	cout << "return 이전" << endl;
+	return ob;
+}
+
+int main() {
+	SoSimple obj(7);
+	SimpleFuncObj(obj).AddNum(30).ShowData();
+	obj.ShowData();
+	return 0;
+}
+
+/* 
+출력창
+Called SoSimple(const SoSimple& copy)
+return 이전
+Called SoSimple(const SoSimple& copy)
+num: 37
+num: 7
+*/
